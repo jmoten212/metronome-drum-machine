4d085e18cbbb7fcc8897ead82d6a70734a0055a4
James <53585345+jmoten212@users.noreply.github.com>
2026-08-10 15:55:41 -0400

    Updated target for repo link

--- src/components/RepoLink.jsx
+++ src/components/RepoLink.jsx
@@ -5,7 +5,7 @@ function RepoLink () {
   return (
     <>
       <div className="repo-link">
-        <a href="https://github.com/jmoten212/metronome-drum-machine" data-tooltip-id="gh-repo-link" data-tooltip-place="right"> 
+        <a href="https://github.com/jmoten212/metronome-drum-machine" data-tooltip-id="gh-repo-link" data-tooltip-place="right" target="_blank"> 
           <img src="https://www.svgrepo.com/show/303548/git-icon-logo.svg" alt="Git icon" className="git-icon"/>
         </a>
       </div>
